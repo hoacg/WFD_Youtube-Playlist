41bbaf900030e4b24057e0417acb1f7a7692d06e
Hoa <danghuyhoa@gmail.com>
2019-10-25 14:17:28 +0700

    Get id from params

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
   },
 
   {
-    path: 'video/:id', component: VideoComponent
+    path: 'video/:videoId', component: VideoComponent
   }
 ];
 
--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-video',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VideoComponent implements OnInit {
 
-  constructor() { }
+  id: string;
+
+
+  constructor(private activatedRoute: ActivatedRoute) {
+
+    this.activatedRoute.params.subscribe( params => {
+      this.id = params.videoId;
+    });
+
+  }
 
   ngOnInit() {
   }
